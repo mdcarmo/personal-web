b1c6f51efc32246acc47a41fec3098a86969c157
marc29dias <marc29dias@gmail.com>
2018-08-25 23:48:39 -0300

    inclusão da pagina de clientes - lista

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -40,6 +40,10 @@ const routes: Routes = [
             {
                 path: 'blank-page',
                 loadChildren: './blank-page/blank-page.module#BlankPageModule'
+            },
+            {
+                path: 'client',
+                loadChildren: './client-list/client-list.module#ClientListModule'
             }
         ]
     }
